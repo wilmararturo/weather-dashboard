d79e6d1a98ba959c64537929bc3b3e51ae44b089
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-09-16 01:49:50 -0400

    current conditions working

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,19 +1,63 @@
 var cityInputEl = $("#cityNameInput");
 var searchButtonEl = $("#search-button");
 var cityListEl = $("#city-list");
-var cityConditionIconEl = $("condition-icon");
+var cityHeaderEl = $("#city-header");
+var cityConditionIconEl = $("#condition-icon");
+var cityConditionIconImgEl = $("#condition-icon-img");
 var cityTemperatureEl = $("#city-temerature");
 var cityHumidityEl = $("#city-humidity");
 var cityWindSpeedEl = $("#city-windspeed");
 var cityUVIndexEl = $("#city-uv-index");
+var cityUVIndexBadgeEl = $("<span>").addClass("badge");
+
+var baseURL = "https://api.openweathermap.org/data/2.5/";
+var apiKey = "37b169c6c553e6d620b06f52f0e2a54e";
+
+
+function renderUVButton(uvData) {
+    console.log(uvData);
+    var uvIndex = Math.round(uvData.value);
+    cityUVIndexBadgeEl.text(uvIndex);
+    if (uvIndex < 3) {
+        cityUVIndexBadgeEl.addClass("badge-success");
+    } else if (2 < uvIndex > 8) {
+        cityUVIndexBadgeEl.addClass("badge-warning");
+    } else if (uvIndex > 7) {
+        cityUVIndexBadgeEl.addClass("badge-danger");
+    }
+}
+
+function getUVIndex(lat, lon) {
+    var queryURL = baseURL + "uvi?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(renderUVButton);
+}
+function updateCurrentConditions(weatherData) {
+    console.log(weatherData);
+    var conditionIconHtml = "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png";
+    var lat = weatherData.coord.lat;
+    var lon = weatherData.coord.lon;
+    var uvBadge = getUVIndex(lat, lon);
+    cityConditionIconImgEl
+        .attr("src", conditionIconHtml)
+        .attr("alt", weatherData.weather[0].description)
+        .appendTo(cityConditionIconEl);
+    cityHeaderEl.text(weatherData.name).append(cityConditionIconEl);
+    cityTemperatureEl.text(`Temperature: ${weatherData.main.temp}`);
+    cityHumidityEl.text(`Humidity: ${weatherData.main.humidity}`);
+    cityWindSpeedEl.text(`Wind Speed: ${weatherData.wind.speed}`);
+    cityUVIndexEl.text("UV Index:").append(cityUVIndexBadgeEl);
+
+}
 
 function buildQueryURL(cityName, forcastType) {
     var queryURL = "";
-    var baseURL = "https://api.openweathermap.org/data/2.5/";
-    var apiKey = "37b169c6c553e6d620b06f52f0e2a54e";
     queryURL = baseURL + forcastType +
         "?q=" + cityName +
         "&appid=" + apiKey;
+    console.log(queryURL);
     return queryURL;
 
 }
@@ -23,11 +67,7 @@ function getOpenWeather(cityName, forcastType) {
     $.ajax({
         url: queryURL,
         method: "GET"
-    })
-
-}
-
-function updateCurrentConditions() {
+    }).then(updateCurrentConditions);
 
 }
 
@@ -35,3 +75,10 @@ $(document).ready(function () {
 
 
 });
+
+searchButtonEl.on("click", function (event) {
+    event.preventDefault();
+
+    var cityName = cityInputEl.val().trim();
+    getOpenWeather(cityName, "weather");
+})
